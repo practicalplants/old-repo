330d957d5e1406c7614dadaf1ea8dbf933ad7730
Andru Vallance <andru@tinymighty.com>
2012-08-11 17:57:05 +0200

    Added loading icon and half second delay to the query form.

--- html/w/extensions/PracticalPlants/modules/ext.practicalplants/js/practicalplants.init.mast-search.js
+++ html/w/extensions/PracticalPlants/modules/ext.practicalplants/js/practicalplants.init.mast-search.js
@@ -19,54 +19,59 @@
 		initAutocomplete();
 	}
 	
+	function doQuery(request, response){
+		var term = request.term.replace(' ','*');
+		termVariants = [];
+		termVariants.push(term);
+		//termVariants.push(term.toLowerCase());
+		//termVariants.push(term.slice(0,1).toUpperCase()+term.slice(1));
+		var query = '';
+		query+='[[Concept:Plant%20taxonomies]][[Has%20taxonomy%20name::~*'+term+'*]]';
+		query+=' OR [[Concept:Plant%20taxonomies]][[Has%20common%20name::~*'+term+'*]]';
+		query+='|%3FIs%20taxonomy%20type|%3FHas%20common%20name|limit=50&format=json';
+		mw.log(query);
+		//mw.log('Term variants:', '[[Has%20taxonomy%20name::~*'+termVariants.join('*]] OR [[Has%20taxonomy%20name::~*')+'*]]');
+		
+		/*
+		query is:
+		has taxonomy name: aPPle Apple apple
+		*/
+		
+		$.getJSON('/w/api.php?action=taxonomies&query='+query).then(function(data){ 
+			//console.log(data.items);
+			mw.log('Data is',data);
+			if(data){
+				results = [];
+				var name,taxo,common;
+				for(var k in data){
+					if(data.hasOwnProperty(k)){
+						name = '<span class="name">'+k+'</span>';
+						common = ' <span class="common">'+data[k].common+'</span>';
+						taxo = ' <span class="taxonomy">'+data[k].taxonomy+'</span>';
+						results.push({label: name+common+taxo, value:k});
+					}
+				}
+				mw.log('Responding with results',results);
+				response(results);
+				search_input.removeClass('loading');
+			}
+		});
+	}
 	
 	function initAutocomplete(){
 		var taxos = [],
-			termVariants = [];
-		
-		//console.log(taxos);
+			termVariants = [],
+			timeout;
 		$(search_input).autocomplete({
 			html: true,
-			delay: 20,
+			delay: 0, //this delay doesn't seem to work, so we recreate it in the source method
 			minLength: 3,
 			position: { my : "center top", at: "center bottom"},//, offset:'2 0' },
 		    source: function(request, response){
-		    	var term = request.term.replace(' ','*');
-		    	termVariants = [];
-		    	termVariants.push(term);
-		    	//termVariants.push(term.toLowerCase());
-		    	//termVariants.push(term.slice(0,1).toUpperCase()+term.slice(1));
-				var query = '';
-				query+='[[Concept:Plant%20taxonomies]][[Has%20taxonomy%20name::~*'+term+'*]]';
-				query+=' OR [[Concept:Plant%20taxonomies]][[Has%20common%20name::~*'+term+'*]]';
-				query+='|%3FIs%20taxonomy%20type|%3FHas%20common%20name|limit=50&format=json';
-				mw.log(query);
-				//mw.log('Term variants:', '[[Has%20taxonomy%20name::~*'+termVariants.join('*]] OR [[Has%20taxonomy%20name::~*')+'*]]');
 		    	
-		    	/*
-		    	query is:
-		    	has taxonomy name: aPPle Apple apple
-		    	*/
-		    	
-		    	$.getJSON('/w/api.php?action=taxonomies&query='+query).then(function(data){ 
-		    		//console.log(data.items);
-		    		mw.log('Data is',data);
-		    		if(data){
-		    			results = [];
-		    			var name,taxo,common;
-		    			for(var k in data){
-		    				if(data.hasOwnProperty(k)){
-		    					name = '<span class="name">'+k+'</span>';
-		    					common = ' <span class="common">'+data[k].common+'</span>';
-		    					taxo = ' <span class="taxonomy">'+data[k].taxonomy+'</span>';
-		    					results.push({label: name+common+taxo, value:k});
-		    				}
-		    			}
-		    			mw.log('Responding with results',results);
-		    			response(results);
-		    		}
-		    	});
-		    	
+		    	//delay query with a timeout. If the user types more within the timeout then the query is cancelled and another is queued. This is how the 'delay' should work for autocomplete but it doesn't...
+		    	clearTimeout(timeout);
+		    	timeout = setTimeout(function(){ doQuery(request,response); },500);
 		    
 		    },
 		   	
@@ -76,6 +81,9 @@
 			select: function(event, ui) { 
 				//console.log(ui.item);
 				search_input.val(ui.item.value).parents('form').submit();
+			},
+			search: function(event, ui){
+				search_input.addClass('loading');
 			}
 		});
 	}
