65ac877750af0a868ff8e3730f844dbda77c2172
Andru Vallance <andru@tinymighty.com>
2012-11-05 13:50:24 +0100

    Updated MoveToSkin to latest version to fix display bug and enable parserCache to be turned back on.

--- html/w/extensions/MoveToSkin/MoveToSkin.php
+++ html/w/extensions/MoveToSkin/MoveToSkin.php
@@ -6,40 +6,37 @@
  * Intended for MediaWiki Skin designers.
  * By Andru Vallance - andru@tinymighty.com
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * http://www.gnu.org/copyleft/gpl.html
+ * License: GPL - http://www.gnu.org/copyleft/gpl.html
  *
  */
 
 $wgHooks['ParserFirstCallInit'][] = 'MoveToSkin::parserFirstCallInit';
 $wgHooks['LanguageGetMagic'][] = 'MoveToSkin::languageGetMagic';
-$wgHooks['ParserAfterTidy'][] = 'MoveToSkin::moveContent';
+/* This was hooked to ParserAfterTidy, but that isn't called when the parse skep
+is skipped by retrieving from the parser cache */
+$wgHooks['OutputPageBeforeHTML'][] = 'MoveToSkin::moveContent';
 
+$wgExtensionCredits['parserhook'][] = array(
+   'path' => __FILE__,
+   'name' => 'Move To Skin',
+   'description' => 'Move content from the article to the skin.',
+   'version' => 0.1.2, 
+   'author' => 'Andru Vallance',
+   'url' => 'https://www.mediawiki.org/wiki/Extension:MoveToSkin'
+);
 
 class MoveToSkin{
   
   public static $content = array();
   
   public static function parserFirstCallInit(&$parser){
-  	$parser->setFunctionHook('movetoskin', 'MoveToSkin::parserFunction');
-  	return true;
+    $parser->setFunctionHook('movetoskin', 'MoveToSkin::parserFunction');
+    return true;
   }
   
   public static function languageGetMagic(&$magicWords){
-  	$magicWords['movetoskin'] = array(0,'movetoskin');
-  	return true;
+    $magicWords['movetoskin'] = array(0,'movetoskin');
+    return true;
   }
   
   public static function parserFunction($parser, $name='', $content=''){
@@ -50,10 +47,10 @@ class MoveToSkin{
     return array( $content, 'noparse' => false, 'isHTML' => false );
   }
   
-  public static function moveContent(&$parser, &$html){
+  public static function moveContent(&$out, &$html){
    if(empty($html))
      return true;
-    //not sure why, but we have to UTF8 decode the html output... is it re-encoding it to non utf8?
+    //not sure why, but we have to UTF8 decode the html output... DOM Document is UTF8, so not sure why this is necessary, but without it UTF8 entities are garbled
     $html = utf8_decode($html);
     $doc = @DOMDocument::loadHTML( $html );
     //$doc->encoding = 'utf8';
